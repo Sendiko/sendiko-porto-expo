9be2e27cbe9aa3f064b69cdcb3935d6787c9729c
Sendiko <sarangtawon897@gmail.com>
2023-12-23 11:30:03 +0700

    enc(add): one more chip and chip container

--- ui/components/HeaderProfile.js
+++ ui/components/HeaderProfile.js
@@ -16,10 +16,11 @@ const HeaderProfile = () => {
                     You can support me by following my Github account.
                 </Text>
                 <View style={{ padding: "1rem"}}/>
-                <View style={globalStyles.rowContainer}>
+                <View style={globalStyles.chipContainer}>
                     <SmallCardLink text="Github" link="https://www.github.com/Sendiko" name="logo-github"/>
                     <SmallCardLink text="LinkedIn" link="https://www.linkedin.com/in/rizky-sendiko-b69563280" name="logo-linkedin"/>
                     <SmallCardLink text="Email" link="mailto:rizkysendiko7@gmail.com" name="mail-outline"/>
+                    <SmallCardLink text="Whatsapp" link="https://wa.link/okhgr0" name="logo-whatsapp"/>
                 </View>
             </View>
         </View>
