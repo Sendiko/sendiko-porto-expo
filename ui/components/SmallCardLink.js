d4dbd8d3dbcd953385940634cbe541d4269bd28e
Sendiko <sarangtawon897@gmail.com>
2023-12-23 11:29:44 +0700

    enc(add): hover effects

--- ui/components/SmallCardLink.js
+++ ui/components/SmallCardLink.js
@@ -1,17 +1,33 @@
-import { Linking,  } from "react-native-web";
+import { Linking, View,  } from "react-native-web";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import globalStyles from "../styles/globalstyles";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react";
 
 
 const CardLink = (props) => {
+    const [isHovered, setHovered] = useState(false)
+
     const handleOnPress = () => {
         Linking.openURL(props.link);
     }
     return(
-        <TouchableOpacity style={globalStyles.linkCard} onPress={handleOnPress}>
-            <Ionicons name={props.name} size={"1.5rem"} style={globalStyles.linkIcon}/>
-            {props.text}
+        <TouchableOpacity onPress={handleOnPress}>
+            <View 
+            style={[
+                globalStyles.linkCard,
+                isHovered && globalStyles.linkCardHover
+            ]} 
+            onMouseEnter={
+                () => {setHovered(true)}
+            }
+            onMouseLeave={
+                () => {setHovered(false)}
+            }
+            >
+                <Ionicons name={props.name} size={"1.5rem"} style={globalStyles.linkIcon}/>
+                {props.text}
+            </View>
         </TouchableOpacity>
     );
 }
